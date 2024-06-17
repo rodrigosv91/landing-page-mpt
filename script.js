366d020eb1e1d8aa8dea60c102b6376a0b09c7ec
rodrigo <rodrigosv91@gmail.com>
2024-06-16 22:14:41 -0300

    add: versão 1

---
+++ script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Função de chamada para ação para "Saiba Mais"
+  document
+    .getElementById("cta-button-saiba-mais")
+    .addEventListener("click", function () {
+      document
+        .getElementById("produtos-servicos")
+        .scrollIntoView({ behavior: "smooth" });
+    });
+
+  // Função de chamada para ação para "Contato"
+  document
+    .getElementById("cta-button-contato")
+    .addEventListener("click", function () {
+      document
+        .getElementById("sobre-contato")
+        .scrollIntoView({ behavior: "smooth" });
+    });
+
+  // Validação do formulário de contato
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault(); // Prevenir envio padrão
+
+      // Coletar dados do formulário
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+      const mensagem = document.getElementById("mensagem").value;
+
+      // Simular envio (exemplo: exibir uma mensagem de agradecimento)
+      alert(
+        `Obrigado pela mensagem, ${nome}! Entraremos em contato através do email ${email}.`
+      );
+
+      // Limpar formulário
+      document.getElementById("contact-form").reset();
+    });
+});
