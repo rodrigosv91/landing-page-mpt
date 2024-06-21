ea83222f8e8626d7b166f8ed611c8946ef14e26a
rodrigo <rodrigosv91@gmail.com>
2024-06-20 21:54:12 -0300

    ajuste estilo

--- script.js
+++ script.js
@@ -1,65 +1,64 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // Função de chamada para ação para "Saiba Mais"
-  document
-    .getElementById("cta-button-saiba-mais")
-    .addEventListener("click", function () {
-      document
-        .getElementById("produtos-servicos")
-        .scrollIntoView({ behavior: "smooth" });
-    });
+  const ctaButtonSaibaMais = document.getElementById("cta-button-saiba-mais");
+  const ctaButtonContato = document.getElementById("cta-button-contato");
 
-  // Função de chamada para ação para "Contato"
-  document
-    .getElementById("cta-button-contato")
-    .addEventListener("click", function () {
-      document
-        .getElementById("sobre-contato")
-        .scrollIntoView({ behavior: "smooth" });
-    });
+  function toggleButtonClasses() {
+    ctaButtonSaibaMais.classList.toggle("btn-custom-rubro");
+    ctaButtonSaibaMais.classList.toggle("btn-custom-ice");
+    ctaButtonContato.classList.toggle("btn-custom-rubro");
+    ctaButtonContato.classList.toggle("btn-custom-ice");
+  }
 
-  // Validação do formulário de contato
-  document
-    .getElementById("contact-form")
-    .addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevenir envio padrão
-
-      // Coletar dados do formulário
-      const nome = document.getElementById("nome").value;
-      const email = document.getElementById("email").value;
-
-      // Simular envio (exemplo: exibir uma mensagem de agradecimento)
-      alert(
-        `Obrigado pela mensagem, ${nome}! Entraremos em contato através do email ${email}.`
-      );
-
-      // Limpar formulário
-      document.getElementById("contact-form").reset();
-    });
-
-  // Alternar estilo
   document
     .getElementById("toggle-style")
     .addEventListener("click", function () {
       const header = document.querySelector("header");
       const produtosServicos = document.getElementById("produtos-servicos");
       const sobreContato = document.getElementById("sobre-contato");
-      const toggleButton = document.getElementById("toggle-style");
-      const ctaButtonSaibaMais = document.getElementById(
-        "cta-button-saiba-mais"
-      );
-      const ctaButtonContato = document.getElementById("cta-button-contato");
 
       header.classList.toggle("gray-ice-header");
       produtosServicos.classList.toggle("bg-gray");
       sobreContato.classList.toggle("bg-ice");
 
+      toggleButtonClasses();
+
+      const toggleButton = document.getElementById("toggle-style");
       if (header.classList.contains("gray-ice-header")) {
         toggleButton.textContent = "Original Style";
       } else {
         toggleButton.textContent = "Toggle Style";
       }
 
-      ctaButtonSaibaMais.classList.toggle("btn-dark");
-      ctaButtonContato.classList.toggle("btn-dark");
+      const formLabels = document.querySelectorAll("#contact-form label");
+      formLabels.forEach((label) => {
+        label.classList.toggle("text-dark");
+      });
+    });
+
+  ctaButtonSaibaMais.addEventListener("click", function () {
+    document
+      .getElementById("produtos-servicos")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+
+  ctaButtonContato.addEventListener("click", function () {
+    document
+      .getElementById("sobre-contato")
+      .scrollIntoView({ behavior: "smooth" });
+  });
+
+  document
+    .getElementById("contact-form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+
+      const nome = document.getElementById("nome").value;
+      const email = document.getElementById("email").value;
+
+      alert(
+        `Obrigado pela mensagem, ${nome}! Entraremos em contato através do email ${email}.`
+      );
+
+      document.getElementById("contact-form").reset();
     });
 });
